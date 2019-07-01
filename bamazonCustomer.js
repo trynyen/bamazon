b251ad83e14fa12615deb95b70338363873cbd49
trynyen <tryn.yen@gmail.com>
2019-07-01 00:02:58 -0500

    added removing quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+//Connecting mysql database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,9 +13,12 @@ var connection = mysql.createConnection({
 
 
 function afterConnection() {
+    //Get data and display on terminal
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.table(res);
+
+        //Displaying 2 questions
         inquirer.prompt([
             {
                 type: "input",
@@ -27,10 +31,19 @@ function afterConnection() {
                 name: "quantity"
             },
         ]).then(function (check) {
+            //Check quantity of product
             for (var i = 0; i < res.length; i++) {
                 if (check.item == res[i].id) {
                     if (res[i].stock_quantity >= check.quantity) {
                         console.log("Fortunately, we have enough stock")
+                        connection.query("DELETE FROM products WHERE?",
+                            {
+                                id: res[i]
+                            },
+                            function (error, response) {
+                                if (error) throw error;
+                                console.log(response.affectedRows + " products deleted!");
+                            })
                     }
                     else {
                         console.log("Insufficient quantity!")
